bfd76ccca21a5810bc8c1b5b602ac7868e6b889d
NguyenHuuHung <hung0968374@gmail.com>
2021-03-27 16:56:54 +0700

    m

--- src/App.js
+++ src/App.js
@@ -1,10 +1,21 @@
 import "./App.css";
+import React, { useState, useEffect } from "react";
 import Auth from "./components/Auth";
 import Header from "./components/Header";
 import Slogan from "./components/Slogan";
 import Posts from "./components/Posts";
 import Footer from "./components/Footer";
+import * as api from "./api";
 function App() {
+  const [allData, setAllData] = useState([]);
+  useEffect(() => {
+    async function getAllData() {
+      let { data } = await api.fetchPosts();
+      setAllData(data);
+    }
+    getAllData();
+  }, []);
+  console.log(allData);
   return (
     <div className="App">
       <Header />
